88a52e76e1e9c7a8f91f136babac4314d46ab985
Abraham Newton Adade <45490207+nabjet@users.noreply.github.com>
2020-12-06 19:42:12 +0000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,24 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+         t <-  NULL
+   set <- function(y)
+   {
+     x <<- y
+     t <<- NULL
+   }
+   
+   get_mat <- function()x
+   
+   set_inv <- function(inv_mat) m <<- inv_mat
+   
+   get_inv <- function() t
+   
+   list(set  = set, get_mat = get_mat, set_inv = set_inv, get_inv = get_inv)
+   
+   
+   
+
 
 }
 
@@ -12,4 +30,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        t <- x$get_inv()
+  if (!is.null(t))
+  {
+    return(t)
+  }
+ 
+  mate <- x$get()
+  
+   t <- solve(x,...)
+   x$set_inv(t)
+    t
 }
