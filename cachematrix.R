14f2436c587767f94f1708a5ac21db4c17c042c9
nabjet <nabjet20@gmail.com>
2020-12-06 20:39:24 +0000

    adding explanatory comments to the functions created

--- cachematrix.R
+++ cachematrix.R
@@ -4,8 +4,8 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-   t <-  NULL
-   set <- function(y)
+   t <-  NULL   
+   set <- function(y) 
    {
      x <<- y
      t <<- NULL
@@ -18,17 +18,21 @@ makeCacheMatrix <- function(x = matrix()) {
    get_inv <- function() t
    
    list(set  = set, get_mat = get_mat, set_inv = set_inv, get_inv = get_inv)
-   
-   
-   
-
 }
+
+## makeCache creates and caches a value variable which would contain t which would 
+## contain the cached value of the inverse of a matrix
+## 'makeCache' then  returns a list ofuf nctions which are used to set the value of
+## matrix, get the value of a matrix, sets the value of the inverse of 
+## a matrix and gets the inverse of a matrix. The funtions returned are 
+## subsequently used by Cachesolve
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x , ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
   t <- x$get_inv()
   if (!is.null(t))
   {
@@ -40,7 +44,11 @@ cacheSolve <- function(x , ...) {
    t <- solve(x,...)
    x$set_inv(t)
     t
-  
-   
      
 }
+
+## upon being provided with a matrix x, cacheSolve first tries to get the 
+## inverse of the matrix in a cached variable. if this exits, it return the value
+## however if the matrix has no cached inverse stored, the inverse is
+## calculated with the solve function. this calculated inverse is then cached and 
+## returned
